caf1ff919b56c4f0167faf770edf34698569bcd6
Brent <Bmilan24@gmail.com>
2017-06-30 11:45:04 -0400

    genre class updated

--- src/main/java/production/Genre.java
+++ src/main/java/production/Genre.java
@@ -6,13 +6,31 @@ import javax.persistence.OneToMany;
 
 public class Genre {
 	
-	
+
 	private Long id;
+	private String title;
 	
 	@OneToMany(mappedBy = "genre")
-	private Set<Review> review;
+	private Set<Review> reviews;
 	
-	public Genre() {
+	private Genre() {
 		
+	}
+	
+	public Genre(String title) {
+		this.title = title;
+		
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public Set<Review> getReviews() {
+		return reviews;
 	}
 }
