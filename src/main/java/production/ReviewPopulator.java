62c13c0e10a5624f5e9bfc01ea6aa74bf6af2f46
Brent <Bmilan24@gmail.com>
2017-07-07 15:36:01 -0400

    tags now display for onereview html page

--- src/main/java/production/ReviewPopulator.java
+++ src/main/java/production/ReviewPopulator.java
@@ -81,7 +81,7 @@ public class ReviewPopulator implements CommandLineRunner {
 						"\r\n" + 
 						"Quo democritum elaboraret ne, has solum inermis in. Quidam fabulas urbanitas ad his, te iudico scaevola ullamcorper sit, stet convenire cotidieque nam at. At mel malis facilisis sententiae, ei malorum maiorum sit, vidisse antiopam ex vim. Ea mea legimus pertinax persequeris. Usu posse nihil deseruisse no, ut aliquam sadipscing pro. Eam no minim denique mediocrem, dicta veniam laboramus eum id.\r\n" + 
 						"\r\n" + 
-						"Mel prima laudem at. Sed ut quodsi prompta facilisis, cum ad congue iisque, sit veritus docendi maluisset et. Nam id semper efficiendi, justo salutatus concludaturque ex vel. Populo melius no usu, ut nam albucius vituperata. Dolore offendit erroribus ei vel.", "/images/eva-mari.png", dystopian, postApoc );
+						"Mel prima laudem at. Sed ut quodsi prompta facilisis, cum ad congue iisque, sit veritus docendi maluisset et. Nam id semper efficiendi, justo salutatus concludaturque ex vel. Populo melius no usu, ut nam albucius vituperata. Dolore offendit erroribus ei vel.", "/images/eva-mari.png", dystopian, postApoc, cult);
 		reviewRepo.save(review2);
 		
 		Review review3 = new Review (drama, "No Country For Old Men", "Lorem ipsum dolor sit amet, audire vituperatoribus pro ad, at facilisi pertinax nam. Sea error dicunt consectetuer ei, vel pertinax rationibus cu, primis nostrud accusamus ea has. Pri ornatus iracundia ea, decore luptatum ea pri. Quidam legendos constituto ad quo, splendide gloriatur contentiones sit ad, minim impetus cum et. Viris nonumy principes vix ne, eu mea modus quodsi.\r\n" + 
